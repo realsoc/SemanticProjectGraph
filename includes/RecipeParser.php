7993463bbb98933473642ccad3f3764286735789
realsoc <hugo.djemaa@gmail.com>
2015-08-06 22:53:55 +0200

    done

--- includes/RecipeParser.php
+++ includes/RecipeParser.php
@@ -18,7 +18,7 @@ class RecipeParser{
 			$results = json_decode($jsonString, true);
 			if (count($results) > 0) {
 				$results = $results["query"]["results"];
-				$title = addslashes($recipe->getTitle());
+				$title = $recipe->getTitle();
 				$jsonRecipe = $results["$title"]["printouts"];
 				echo 'D'.addslashes(array_keys($results)[0])."F<br>";
 				echo 'D'.addslashes($title)."F<br>";
